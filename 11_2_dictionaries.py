bbd28e6699b08018639848d6c7d1bf5648593a66
smurphy2230 <smurphy2230@gmail.com>
2020-04-17 08:53:43 -0400

    using dict function

--- 11_2_dictionaries.py
+++ 11_2_dictionaries.py
@@ -16,12 +16,4 @@ print(hurricanes[14])
 print(hurricanes[74])
 print("Number of Players: ", len(hurricanes))
 print()
-#  ------------------------------------
-#  create a dictionary from a list.
-station_list = [[91.5, 'WUNC'], [94.7, 'WQDR'], [
-    99.9, 'WCMC'], [100.7, 'WRDU'], [105.1, 'WDCG']]
-stations = dict(station_list)
-print("91.5 FM: ", stations[91.5])
-print("94.7 FM: ", stations[94.7])
-print("9909 FM: ", stations[99.9])
-print("Number of stations: ", len(stations))
+
