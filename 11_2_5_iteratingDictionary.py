43429f9bd2bd05c7acd896bb685243e505880143
smurphy2230 <smurphy2230@gmail.com>
2020-04-17 09:12:28 -0400

    iterate through dictionary

---
+++ 11_2_5_iteratingDictionary.py
@@ -0,0 +1,7 @@
+hurricanes = {20: 'Aho', 18: 'Dzingel', 13: 'Foegele', 48: 'Martinook', 23: 'McGinn', 19: 'Hamilton', 88: 'Necas', 21: 'Neiderreiter', 11: 'Staal', 37: 'Svechnikov', 86: 'Teravainen', 16: 'Trochek', 14: 'Williams', 6: 'Edmundson', 4: 'Fleury', 51: 'Gardiner', 22: 'Pesce', 76: 'Skjei', 74: 'Slavin', 57: 'TVR', 45: 'Vatanen', 34: 'Mrazek', 47: 'Reimer', 43: 'Geekie'}
+
+#  for element in hurricanes:
+#      print(element)
+
+for element in hurricanes:
+    print(hurricanes[element])
